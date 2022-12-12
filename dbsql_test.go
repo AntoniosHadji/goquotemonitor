ba1b4d73b183e25c730266d8118f18d5e96eab21
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-12-12 14:00:28 -0500

    working send bid/ask

--- dbsql_test.go
+++ dbsql_test.go
@@ -10,16 +10,34 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
+func init() {
+	fmt.Println("this is init")
+
+	// err = db.Ping()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	fmt.Println("Finish init")
+}
+
 func TestStdLibSQL(t *testing.T) {
+	var err error
+	var db *sql.DB
+
 	// Opening a driver typically will not attempt to connect to the database.
-	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	db, err = sql.Open("pgx", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer db.Close()
+
+	db.SetConnMaxLifetime(0)
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(10)
 
 	var greeting string
 	err = db.QueryRow("select 'Hello, world!'").Scan(&greeting)
@@ -29,21 +47,25 @@ func TestStdLibSQL(t *testing.T) {
 	}
 
 	fmt.Println(greeting)
+
 }
 
 func TestInsert(t *testing.T) {
-	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	var err error
+	var db *sql.DB
+
+	// Opening a driver typically will not attempt to connect to the database.
+	db, err = sql.Open("pgx", os.Getenv("DATABASE_URL"))
 	if err != nil {
+		// This will not be a connection error, but a DSN parse error or
+		// another initialization error.
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer db.Close()
 
 	db.SetConnMaxLifetime(0)
 	db.SetMaxIdleConns(10)
 	db.SetMaxOpenConns(10)
-
-	fmt.Printf("drivers: %v\n", sql.Drivers())
 
 	stmt, err := db.Prepare("INSERT INTO spreads (ts,bid,ask,size,width_bps,ticker,lp) VALUES($1,$2,$3,$4,$5,$6,$7)")
 	if err != nil {
--- requests_test.go
+++ requests_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 	"testing"
 	"time"
 )
@@ -13,6 +14,7 @@ import (
 var p = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoX3NlY3JldCI6ImEzNTUzZDhiLWYyNTYtNGI0My04ZGNhLTM0N2Y0MzkyNTRhYiIsInVzZXJfZ3JvdXBzIjpbImJvdHMiLCJjYXNoX21hbmFnZXJzIiwiY29tcGxpYW5jZV9vZmZpY2VycyIsImN1c3RvbWVyX3NlcnZpY2VfcmVwcyIsImVuZ2luZWVycyIsImludmVzdG1lbnRfb2ZmaWNlcnMiLCJsaXF1aWRpdHlfbWFuYWdlcnMiLCJ0cmFkZV9kZXNrcyIsInNhbGVzX21hbmFnZXJzIiwidHJ1c3Rfb2ZmaWNlcnMiXSwibm93IjoxNjQzMzk2NDk3LCJleHAiOjE2NzI1MzEyMDB9.eLCS46U_4MEy7oKTvMm8X1uH4N2XXZ8T4NU5VLZgfNY"
 var client = &http.Client{}
 var baseurl = "https://api.primetrust.com"
+var version = "v2"
 
 // QuoteResponse struct to store response from /v2/quotes API
 type QuoteResponse struct {
@@ -102,7 +104,7 @@ func TestSendQuoteRequest(t *testing.T) {
 				AccountID:         "0c7715e3-7cdd-4d49-88bb-f1ab3cb8803b",
 				AssetID:           "43454116-7026-4b3d-a9de-eeaada500d4c",
 				Hot:               false,
-				TransactionType:   "buy",
+				TransactionType:   "sell",
 				UnitCount:         1.0,
 				TradeDeskID:       "4c248890-703d-4ac3-9ce7-9de6465f328a",
 				DelayedSettlement: true,
@@ -111,27 +113,56 @@ func TestSendQuoteRequest(t *testing.T) {
 	}
 	fmt.Println(reqdata)
 
-	pb, err := json.Marshal(APIReq)
-	fmt.Println(string(pb))
-	if err != nil {
-		_ = fmt.Errorf("Error: %v", err)
-	}
-	send("POST", fmt.Sprintf("%s/v2/quotes", baseurl), bytes.NewBuffer(pb))
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	pb, err = json.Marshal(reqdata)
-	fmt.Println(string(pb))
-	if err != nil {
-		_ = fmt.Errorf("Error: %v", err)
+	ch := make(chan QuoteResponse, 2)
+
+	start := time.Now()
+	// Code to measure
+	go func() {
+		defer wg.Done()
+
+		pb, err := json.Marshal(APIReq)
+		if err != nil {
+			_ = fmt.Errorf("Error: %v", err)
+		}
+		r := send("POST", fmt.Sprintf("%s/%s/quotes", baseurl, version), bytes.NewBuffer(pb))
+		ch <- r
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		pb, err := json.Marshal(reqdata)
+		if err != nil {
+			_ = fmt.Errorf("Error: %v", err)
+		}
+		r := send("POST", fmt.Sprintf("%s/%s/quotes", baseurl, version), bytes.NewBuffer(pb))
+		ch <- r
+	}()
+
+	fmt.Println("Waiting...")
+	wg.Wait()
+	duration := time.Since(start)
+	close(ch)
+	// Formatted string, such as "2h3m0.5s" or "4.503μs"
+	fmt.Printf("duration: %v\n", duration)
+
+	for r := range ch {
+		fmt.Println(r)
+		fmt.Println(r.Data.Attributes.PricePerUnit)
 	}
-	send("POST", fmt.Sprintf("%s/v2/quotes", baseurl), bytes.NewBuffer(pb))
+	fmt.Println("Finished loop over channel")
+
 }
 
-func send(method, url string, payload io.Reader) {
+func send(method, url string, payload io.Reader) QuoteResponse {
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		fmt.Println("request error")
 		fmt.Println(err)
-		return
+		return QuoteResponse{}
 	}
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(p)))
 	req.Header.Add("Content-Type", "application/json")
@@ -140,22 +171,22 @@ func send(method, url string, payload io.Reader) {
 	if err != nil {
 		fmt.Println("http client error")
 		fmt.Println(err)
-		return
+		return QuoteResponse{}
 	}
 	fmt.Println(res.Status)
 
+	var r QuoteResponse
 	if res.StatusCode == 201 {
-		var r QuoteResponse
 		if err = json.NewDecoder(res.Body).Decode(&r); err != nil {
 			fmt.Println("unmarshal error")
 			fmt.Println(err)
 		}
-		fmt.Println(r)
-		fmt.Println(r.Data.Attributes.PricePerUnit)
 	} else {
 		fmt.Println(res.Status)
 		fmt.Println(res)
 		fmt.Println(res.Body)
 	}
+
+	return r
 
 }
