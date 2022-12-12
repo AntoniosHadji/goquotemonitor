effb66f32a52d3cdf36c3e09d6445edb72898ae4
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-12-12 15:30:36 -0500

    requests moved to separate files

---
+++ request.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var p = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoX3NlY3JldCI6ImEzNTUzZDhiLWYyNTYtNGI0My04ZGNhLTM0N2Y0MzkyNTRhYiIsInVzZXJfZ3JvdXBzIjpbImJvdHMiLCJjYXNoX21hbmFnZXJzIiwiY29tcGxpYW5jZV9vZmZpY2VycyIsImN1c3RvbWVyX3NlcnZpY2VfcmVwcyIsImVuZ2luZWVycyIsImludmVzdG1lbnRfb2ZmaWNlcnMiLCJsaXF1aWRpdHlfbWFuYWdlcnMiLCJ0cmFkZV9kZXNrcyIsInNhbGVzX21hbmFnZXJzIiwidHJ1c3Rfb2ZmaWNlcnMiXSwibm93IjoxNjQzMzk2NDk3LCJleHAiOjE2NzI1MzEyMDB9.eLCS46U_4MEy7oKTvMm8X1uH4N2XXZ8T4NU5VLZgfNY"
+var client = &http.Client{}
+var baseurl = "https://api.primetrust.com"
+var version = "v2"
+
+// QuoteResponse struct to store response from /v2/quotes API
+type QuoteResponse struct {
+	Data struct {
+		ID         string `json:"id"`
+		Type       string `json:"type"`
+		Attributes struct {
+			AssetName         string    `json:"asset-name"`
+			BaseAmount        float64   `json:"base-amount"`
+			CreatedAt         time.Time `json:"created-at"`
+			DelayedSettlement bool      `json:"delayed-settlement"`
+			ExecutedAt        time.Time `json:"executed-at"`
+			ExpiresAt         time.Time `json:"expires-at"`
+			FeeAmount         float64   `json:"fee-amount"`
+			Hot               bool      `json:"hot"`
+			TradeID           string    `json:"trade-id"`
+			IntegratorSettled bool      `json:"integrator-settled"`
+			PricePerUnit      float64   `json:"price-per-unit"`
+			RejectedAt        time.Time `json:"rejected-at"`
+			SettledAt         time.Time `json:"settled-at"`
+			Status            string    `json:"status"`
+			TotalAmount       float64   `json:"total-amount"`
+			TransactionType   string    `json:"transaction-type"`
+			UnitCount         float64   `json:"unit-count"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
+
+// PTRequestBody payload for request to /v2/quotes API
+type PTRequestBody struct {
+	Data QRData `json:"data"`
+}
+
+// QRData ...
+type QRData struct {
+	Type       string            `json:"type"`
+	Attributes QuoteRequestAttrs `json:"attributes"`
+}
+
+// QuoteRequestAttrs is attributes for Quote request
+type QuoteRequestAttrs struct {
+	AccountID         string  `json:"account-id"`
+	AssetID           string  `json:"asset-id"`
+	TransactionType   string  `json:"transaction-type"`
+	UnitCount         float64 `json:"unit-count"`
+	Hot               bool    `json:"hot,omitempty"`
+	DelayedSettlement bool    `json:"delayed-settlement,omitempty"`
+	TradeDeskID       string  `json:"trade-desk-id,omitempty"`
+}
+
+// ====================================================================
+
+func ptQuoteRequest(payload io.Reader) (*QuoteResponse, error) {
+	path := fmt.Sprintf("%s/%s/quotes", baseurl, version)
+	req, err := http.NewRequest("POST", path, payload)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(p)))
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	fmt.Println(res.Status)
+
+	var r QuoteResponse
+	if res.StatusCode == 201 {
+		if err = json.NewDecoder(res.Body).Decode(&r); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(res.Status)
+		fmt.Println(res)
+		fmt.Println(res.Body)
+	}
+
+	return &r, nil
+
+}
--- requests_test.go
+++ requests_test.go
@@ -4,44 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"sync"
 	"testing"
 	"time"
 )
 
-var p = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoX3NlY3JldCI6ImEzNTUzZDhiLWYyNTYtNGI0My04ZGNhLTM0N2Y0MzkyNTRhYiIsInVzZXJfZ3JvdXBzIjpbImJvdHMiLCJjYXNoX21hbmFnZXJzIiwiY29tcGxpYW5jZV9vZmZpY2VycyIsImN1c3RvbWVyX3NlcnZpY2VfcmVwcyIsImVuZ2luZWVycyIsImludmVzdG1lbnRfb2ZmaWNlcnMiLCJsaXF1aWRpdHlfbWFuYWdlcnMiLCJ0cmFkZV9kZXNrcyIsInNhbGVzX21hbmFnZXJzIiwidHJ1c3Rfb2ZmaWNlcnMiXSwibm93IjoxNjQzMzk2NDk3LCJleHAiOjE2NzI1MzEyMDB9.eLCS46U_4MEy7oKTvMm8X1uH4N2XXZ8T4NU5VLZgfNY"
-var client = &http.Client{}
-var baseurl = "https://api.primetrust.com"
-var version = "v2"
-
-// QuoteResponse struct to store response from /v2/quotes API
-type QuoteResponse struct {
-	Data struct {
-		ID         string `json:"id"`
-		Type       string `json:"type"`
-		Attributes struct {
-			AssetName         string    `json:"asset-name"`
-			BaseAmount        float64   `json:"base-amount"`
-			CreatedAt         time.Time `json:"created-at"`
-			DelayedSettlement bool      `json:"delayed-settlement"`
-			ExecutedAt        time.Time `json:"executed-at"`
-			ExpiresAt         time.Time `json:"expires-at"`
-			FeeAmount         float64   `json:"fee-amount"`
-			Hot               bool      `json:"hot"`
-			TradeID           string    `json:"trade-id"`
-			IntegratorSettled bool      `json:"integrator-settled"`
-			PricePerUnit      float64   `json:"price-per-unit"`
-			RejectedAt        time.Time `json:"rejected-at"`
-			SettledAt         time.Time `json:"settled-at"`
-			Status            string    `json:"status"`
-			TotalAmount       float64   `json:"total-amount"`
-			TransactionType   string    `json:"transaction-type"`
-			UnitCount         float64   `json:"unit-count"`
-		} `json:"attributes"`
-	} `json:"data"`
-}
+// Not Used, only for example purposes ========================================
 
 // QuoteRequest payload for request to /v2/quotes API
 type QuoteRequest struct {
@@ -59,31 +27,15 @@ type QuoteRequest struct {
 	} `json:"data"`
 }
 
-// ====================================================================
-
-// PTRequestBody payload for request to /v2/quotes API
-type PTRequestBody struct {
-	Data QRData `json:"data"`
+// QuoteRequest payload for request to /v2/quotes API
+type QuoteRequest2 struct {
+	Data struct {
+		Type              string `json:"type"`
+		QuoteRequestAttrs `json:"attributes"`
+	} `json:"data"`
 }
 
-// QRData ...
-type QRData struct {
-	Type       string            `json:"type"`
-	Attributes QuoteRequestAttrs `json:"attributes"`
-}
-
-// QuoteRequestAttrs is attributes for Quote request
-type QuoteRequestAttrs struct {
-	AccountID         string  `json:"account-id"`
-	AssetID           string  `json:"asset-id"`
-	TransactionType   string  `json:"transaction-type"`
-	UnitCount         float64 `json:"unit-count"`
-	Hot               bool    `json:"hot,omitempty"`
-	DelayedSettlement bool    `json:"delayed-settlement,omitempty"`
-	TradeDeskID       string  `json:"trade-desk-id,omitempty"`
-}
-
-// ====================================================================
+//=============================================================================
 
 func TestSendQuoteRequest(t *testing.T) {
 	var APIReq = new(QuoteRequest)
@@ -96,6 +48,18 @@ func TestSendQuoteRequest(t *testing.T) {
 	APIReq.Data.Attrs.TradeDeskID = "4c248890-703d-4ac3-9ce7-9de6465f328a"
 	APIReq.Data.Attrs.DelayedSettlement = true
 	fmt.Println(APIReq)
+
+	// embedded attrs
+	var APIReq2 = new(QuoteRequest2)
+	APIReq2.Data.Type = "quotes"
+	APIReq2.Data.AccountID = "0c7715e3-7cdd-4d49-88bb-f1ab3cb8803b"
+	APIReq2.Data.AssetID = "43454116-7026-4b3d-a9de-eeaada500d4c"
+	APIReq2.Data.Hot = false
+	APIReq2.Data.TransactionType = "buy"
+	APIReq2.Data.UnitCount = 1.0
+	APIReq2.Data.TradeDeskID = "4c248890-703d-4ac3-9ce7-9de6465f328a"
+	APIReq2.Data.DelayedSettlement = true
+	fmt.Println(APIReq2)
 
 	var reqdata = &PTRequestBody{
 		QRData{
@@ -116,7 +80,7 @@ func TestSendQuoteRequest(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	ch := make(chan QuoteResponse, 2)
+	ch := make(chan *QuoteResponse, 2)
 
 	start := time.Now()
 	// Code to measure
@@ -125,9 +89,12 @@ func TestSendQuoteRequest(t *testing.T) {
 
 		pb, err := json.Marshal(APIReq)
 		if err != nil {
-			_ = fmt.Errorf("Error: %v", err)
+			fmt.Println(err)
 		}
-		r := send("POST", fmt.Sprintf("%s/%s/quotes", baseurl, version), bytes.NewBuffer(pb))
+		r, err := ptQuoteRequest(bytes.NewBuffer(pb))
+		if err != nil {
+			fmt.Println(err)
+		}
 		ch <- r
 	}()
 
@@ -136,9 +103,12 @@ func TestSendQuoteRequest(t *testing.T) {
 
 		pb, err := json.Marshal(reqdata)
 		if err != nil {
-			_ = fmt.Errorf("Error: %v", err)
+			fmt.Println(err)
 		}
-		r := send("POST", fmt.Sprintf("%s/%s/quotes", baseurl, version), bytes.NewBuffer(pb))
+		r, err := ptQuoteRequest(bytes.NewBuffer(pb))
+		if err != nil {
+			fmt.Println(err)
+		}
 		ch <- r
 	}()
 
@@ -154,39 +124,5 @@ func TestSendQuoteRequest(t *testing.T) {
 		fmt.Println(r.Data.Attributes.PricePerUnit)
 	}
 	fmt.Println("Finished loop over channel")
-
-}
-
-func send(method, url string, payload io.Reader) QuoteResponse {
-	req, err := http.NewRequest(method, url, payload)
-	if err != nil {
-		fmt.Println("request error")
-		fmt.Println(err)
-		return QuoteResponse{}
-	}
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(p)))
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println("http client error")
-		fmt.Println(err)
-		return QuoteResponse{}
-	}
-	fmt.Println(res.Status)
-
-	var r QuoteResponse
-	if res.StatusCode == 201 {
-		if err = json.NewDecoder(res.Body).Decode(&r); err != nil {
-			fmt.Println("unmarshal error")
-			fmt.Println(err)
-		}
-	} else {
-		fmt.Println(res.Status)
-		fmt.Println(res)
-		fmt.Println(res.Body)
-	}
-
-	return r
 
 }
