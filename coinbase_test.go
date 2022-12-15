02eb52b3fa8879d879409286354b62c1f5f39372
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-12-14 20:03:58 -0500

    test extract data from interface

--- coinbase_test.go
+++ coinbase_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 )
 
@@ -11,6 +12,20 @@ func TestCoinbaseQuote(t *testing.T) {
 		t.Errorf("FAIL: %v", err)
 	}
 
-	fmt.Println(res.Bids[0])
-	fmt.Println(res.Asks[0])
+	fmt.Printf("%s %s\n", res.Bids[0][0], res.Bids[0][1])
+	fmt.Printf("%s %s\n", res.Asks[0][0], res.Asks[0][1])
+	fmt.Printf("%T %T\n", res.Asks[0][0], res.Asks[0][1])
+
+	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][0]), 32); err == nil {
+		fmt.Println(s)
+	}
+	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][0]), 64); err == nil {
+		fmt.Println(s)
+	}
+	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][1]), 32); err == nil {
+		fmt.Println(s)
+	}
+	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][1]), 64); err == nil {
+		fmt.Println(s)
+	}
 }
