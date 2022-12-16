025477d0601e853480cab7bcbe3b06f28d13ce5d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-12-16 17:02:08 -0500

    working coinbase calcspread

--- coinbase.go
+++ coinbase.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 var cbBaseURL = "https://api.exchange.coinbase.com"
@@ -17,10 +18,8 @@ type CoinbaseBookResponse struct {
 }
 
 func getBook(ticker string) (*CoinbaseBookResponse, error) {
-	path := fmt.Sprintf("%s/products/%s-USD/book", cbBaseURL, ticker)
-	// query = {"level": 2}
-	//     r = requests.get(f"{BASE_PRO}{path}", params=query).json()
-	req, err := http.NewRequest("GET", path+"?level=2", nil)
+	path := fmt.Sprintf("%s/products/%s-USD/book?level=2", cbBaseURL, ticker)
+	req, err := http.NewRequest("GET", path, nil)
 	if err != nil {
 		log.Println(err)
 		return nil, err
@@ -44,9 +43,46 @@ func getBook(ticker string) (*CoinbaseBookResponse, error) {
 	} else {
 		fmt.Println(res.Status)
 		fmt.Println(res)
-		fmt.Printf("%#v", r)
-		return nil, fmt.Errorf("Bad status: %s\n%#v", res.Status, r)
+		return nil, fmt.Errorf("Bad status: %s", res.Status)
 	}
 
 	return &r, nil
+}
+
+func calcSpread(data *CoinbaseBookResponse, size float64) float64 {
+
+	bid := getSize(data.Bids, size)
+	ask := getSize(data.Asks, size)
+	// debug
+	//fmt.Println(bid, ask)
+
+	return 10000 * (ask - bid) / bid
+
+}
+
+func getSize(data [][]interface{}, size float64) float64 {
+
+	// fmt.Println(len(data), ",psum", ",ssum", ",psum/ssum", ",p", ",s")
+	var p, s, psum, ssum, z float64
+	var err error
+	for i := range data {
+		if s, err = strconv.ParseFloat(fmt.Sprintf("%v", data[i][1]), 64); err == nil {
+			// fmt.Printf("%T: %f -> ", s, s)
+			ssum += s
+		}
+		if p, err = strconv.ParseFloat(fmt.Sprintf("%v", data[i][0]), 64); err == nil {
+			if ssum >= size {
+				z = size - (ssum - s)
+				ssum = ssum - s + z
+				psum += p * z
+				break
+			}
+			psum += p * s
+		}
+		// debug
+		// fmt.Printf("%d, %.8f,%.8f,%.8f,%.8f,%.8f\n", i, psum, ssum, psum/ssum, p, s)
+	}
+	// fmt.Printf("99, %.8f,%.8f,%.8f,%.8f,%.8f\n", psum, ssum, psum/ssum, p, z)
+
+	return psum / ssum
 }
--- coinbase_test.go
+++ coinbase_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 	"testing"
 )
@@ -17,15 +19,37 @@ func TestCoinbaseQuote(t *testing.T) {
 	fmt.Printf("%T %T\n", res.Asks[0][0], res.Asks[0][1])
 
 	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][0]), 32); err == nil {
-		fmt.Printf("%T: %f", s, s)
+		fmt.Printf("%T: %f\n", s, s)
 	}
 	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][0]), 64); err == nil {
-		fmt.Printf("%T: %f", s, s)
+		fmt.Printf("%T: %f\n", s, s)
 	}
 	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][1]), 32); err == nil {
-		fmt.Printf("%T: %f", s, s)
+		fmt.Printf("%T: %f\n", s, s)
 	}
 	if s, err := strconv.ParseFloat(fmt.Sprintf("%v", res.Asks[0][1]), 64); err == nil {
-		fmt.Printf("%T: %f", s, s)
+		fmt.Printf("%T: %f\n", s, s)
 	}
+}
+
+func TestCalcSpread(t *testing.T) {
+	f, e := os.ReadFile("./testdata/testcb.json")
+	if e != nil {
+		t.Errorf("file read failed: %#v\n", e)
+	}
+
+	var data CoinbaseBookResponse
+
+	e = json.Unmarshal([]byte(f), &data)
+	if e != nil {
+		t.Errorf("Unmarshal failed: %#v\n", e)
+	}
+
+	// fmt.Printf("bids: %v\n", data.Bids)
+	// fmt.Printf("asks: %v\n", data.Asks)
+
+	result := calcSpread(&data, 1)
+	fmt.Println(result, "bps")
+	fmt.Printf("%.3f bps\n", result)
+
 }
