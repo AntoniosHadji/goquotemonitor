7f1108ecb6ea3b77696e55d0646b8e03dfbedef0
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-12-17 15:07:30 -0500

    test new request body struct format

--- requests_test.go
+++ requests_test.go
@@ -1,9 +1,63 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"testing"
 )
+
+// PTRequestBody payload for request to /v2/quotes API
+type PTRequestBodyTest struct {
+	Data struct {
+		Type       string            `json:"type"`
+		Attributes QuoteRequestAttrs `json:"attributes"`
+	} `json:"data"`
+}
+
+func TestPTRequestBody(t *testing.T) {
+
+	var reqdata = PTRequestBodyTest{}
+	reqdata.Data.Type = "quotes"
+	reqdata.Data.Attributes = QuoteRequestAttrs{
+		AccountID:         "0c7715e3-7cdd-4d49-88bb-f1ab3cb8803b",
+		AssetID:           "43454116-7026-4b3d-a9de-eeaada500d4c",
+		TransactionType:   "sell",
+		UnitCount:         1.0,
+		Hot:               false,
+		TradeDeskID:       "4c248890-703d-4ac3-9ce7-9de6465f328a",
+		DelayedSettlement: true,
+	}
+
+	var reqdataOrig = PTRequestBody{
+		QRData{
+			Type: "quotes",
+			Attributes: QuoteRequestAttrs{
+				AccountID:         "0c7715e3-7cdd-4d49-88bb-f1ab3cb8803b",
+				AssetID:           "43454116-7026-4b3d-a9de-eeaada500d4c",
+				TransactionType:   "sell",
+				UnitCount:         1.0,
+				Hot:               false,
+				TradeDeskID:       "4c248890-703d-4ac3-9ce7-9de6465f328a",
+				DelayedSettlement: true,
+			},
+		},
+	}
+
+	pb, err := json.Marshal(reqdata)
+	if err != nil {
+		t.Error(err)
+	}
+	pb2, err := json.Marshal(reqdataOrig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if bytes.Compare(pb, pb2) != 0 {
+		fmt.Println(pb)
+		fmt.Println(pb2)
+	}
+}
 
 func TestPrepareQuoteRequest(t *testing.T) {
 
