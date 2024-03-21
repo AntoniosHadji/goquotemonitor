080fe8c364c7a1bd7974728a25d6e4e7faec64dc
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2024-03-21 16:10:01 -0400

    update lp for db

--- kraken/worker.go
+++ kraken/worker.go
@@ -7,6 +7,8 @@ import (
 	"github.com/antonioshadji/goquotemonitor/db"
 )
 
+var provider = "Kraken"
+
 func Work(w db.Work) {
 	for {
 		response, err := getBook(w.Ticker)
@@ -25,14 +27,14 @@ func Work(w db.Work) {
 			Size:   w.Size,
 			Width:  bps,
 			Ticker: w.Ticker,
-			LP:     "Coinbase",
+			LP:     provider,
 		}
 		result, err := db.InsertData(data)
 		if err != nil {
 			log.Println(err)
 		}
 		ra, _ := result.RowsAffected()
-		log.Printf("Inserted %d row for LP: %s ticker: %s size: %f.\n", ra, "Coinbase", w.Ticker, w.Size)
+		log.Printf("Inserted %d row for LP: %s ticker: %s size: %f.\n", ra, provider, w.Ticker, w.Size)
 
 		time.Sleep(60 * time.Second)
 	}
